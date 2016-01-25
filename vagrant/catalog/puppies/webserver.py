63f93451f8434ac01bf6dbb0688347b0872dba99
haredega <hgarcia.ee@gmail.com>
2016-01-25 18:39:43 -0200

    Adds templates for all 3 READ operations on the db

--- vagrant/catalog/puppies/webserver.py
+++ vagrant/catalog/puppies/webserver.py
@@ -27,7 +27,6 @@ def list_view(list_type):
     elif list_type == 'owners':
         items = session.query(Owner).all()
     else:
-        #TODO - create error 404 template
         return render_template('error404.html')
     ans = render_template('list_view.html', list_type = list_type, items = items, now=datetime.date.today() )
     return ans
@@ -35,19 +34,21 @@ def list_view(list_type):
 @app.route('/<string:list_type>/<int:item_id>')
 def item_view(list_type, item_id):
     if list_type == 'puppies':
-        item = session.query(Puppy).filter(id=item_id).first()
+        item = session.query(Puppy, Shelter).filter(Puppy.id==item_id, Puppy.shelter_id==Shelter.id).first()
+        template='puppy_view.html'
     elif list_type == 'shelters':
-        item = session.query(Shelter).filter(id=item_id).first()
+        item = session.query(Shelter).filter(Shelter.id==item_id).first()
+        template = 'shelter_view.html'
     elif list_type == 'owners':
-        item = session.query(Owner).filter(id=item_id).first()
+        item = session.query(Owner).filter(Owner.id==item_id).first()
+        template= 'owner_view.html'
     else:
-        #TODO - create error 404 template
         return render_template('error404.html')
     if item is None:
         flash("Could not find the "+list_type+"!")
-        return redirect(url_for('/'+list_type+'/list'))
+        return redirect(url_for('list_view', list_type=list_type))
     else:
-        ans = render_template('item_view.html', list_type=list_type, item_id = item_id, item = item)
+        ans = render_template(template, list_type=list_type, item_id = item_id, item = item)
         return ans
 
 @app.route('/<string:list_type>/new', methods=['GET', 'POST'])
